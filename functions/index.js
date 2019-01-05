f279ee5beb2a981262423368713c21c2e2f94e46
ben <bencrane5.12@gmail.com>
2019-01-05 09:50:02 -0500

    Implement user creation function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,9 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp(functions.config().firebase);
+
+var db = admin.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -8,5 +13,9 @@ const functions = require("firebase-functions");
 // });
 
 exports.createUserDocument = functions.auth.user().onCreate(user => {
-  console.log(user);
+  var docRef = db.collection("users").doc(user.uid);
+  var setUser = docRef.set({
+    todoistLinked: false
+  });
+  return setUser;
 });
