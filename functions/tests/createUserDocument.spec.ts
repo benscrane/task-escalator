20aa417313d62add38a075dd8a8e28a767917739
Ben Crane <bencrane5.12@gmail.com>
2021-04-07 21:39:50 -0500

    add unit test

--- functions/tests/createUserDocument.spec.ts
+++ functions/tests/createUserDocument.spec.ts
@@ -1,8 +1,58 @@
 import 'jest';
 import './helpers/mockFirebaseSetup';
+import * as createUserDocument from '../src/createUserDocument';
+import { db } from '../src/admin';
 
 describe('Module: createUserDocument', () => {
-    it('placeholder', () => {
-        expect(true).toEqual(true);
+    describe('Function: createUserDocument', () => {
+        let docMock: any;
+        let setMock: any;
+        let collectionSpy: jest.SpyInstance;
+
+        const user: createUserDocument.TaskalatorUser = {
+            uid: 'test',
+        };
+
+        beforeEach(() => {
+            setMock = jest.fn().mockResolvedValue({});
+
+            docMock = jest.fn()
+                .mockReturnValue({
+                    set: setMock,
+                });
+
+            collectionSpy = jest.spyOn(db, 'collection')
+                .mockReturnValue({
+                    doc: docMock,
+                } as any);
+        });
+
+        afterEach(() => {
+            jest.restoreAllMocks();
+        });
+
+        it('should call doc with user id', async () => {
+            await createUserDocument.createUserDocument(user);
+
+            expect(docMock.mock.calls[0][0]).toEqual('test');
+        });
+
+        it('should call collection with users', async () => {
+            await createUserDocument.createUserDocument(user);
+
+            expect(collectionSpy.mock.calls[0][0]).toEqual('users');
+        });
+
+        it('should call set appropriately', async () => {
+            await createUserDocument.createUserDocument(user);
+
+            expect(setMock.mock.calls[0]).toEqual([
+                {
+                    todoistLinked: false,
+                }, {
+                    merge: true,
+                },
+            ]);
+        })
     });
 });
