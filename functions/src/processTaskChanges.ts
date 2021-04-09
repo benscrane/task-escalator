ebbd4087f931ea377b2f2081a5a999299a5404f2
Ben Crane <bencrane5.12@gmail.com>
2021-04-09 14:37:36 -0500

    add processTaskChanges tests

--- functions/src/processTaskChanges.ts
+++ functions/src/processTaskChanges.ts
@@ -3,36 +3,30 @@ import { Request, Response } from 'express';
 import * as _ from 'lodash';
 import { rollbar } from './admin';
 
-const pubsub = new PubSub();
-
-// TODO: make interface that extends Request
-
 export const processTaskChanges = async (req: Request, res: Response) => {
   const todoistId: string = _.get(req, 'body.user_id');
-  // const luckyNumber = Math.floor(Math.random() * 10);
-  // if (luckyNumber === 5) {
-  //   const todoistInitiator = _.get(req.body, 'initiator');
-  //   rollbar.info('Todoist user information', {
-  //     todoistInitiator,
-  //   });
-  // }
-  const topic: string = 'todoist-updates';
   if (!todoistId) {
-    res.status(500).send();
+    res.status(400).send();
+    return;
   }
-  // TODO: does res.send prevent the below from being executed? that should be a unit test
 
+  const pubsub = getPubSub();
+
+  const topic: string = 'todoist-updates';
   const data = {
     todoistId,
   };
   const dataBuffer = Buffer.from(JSON.stringify(data));
+
   try {
     await pubsub.topic(topic).publish(dataBuffer);
   } catch (error) {
     rollbar.error('Failed to publish user ID from task to pubsub', {
       error,
     });
-    // TODO: should probably call res.send with an error here
   }
+
   res.status(200).send();
 };
+
+export const getPubSub = () => new PubSub();
--- functions/tests/processTaskChanges.spec.ts
+++ functions/tests/processTaskChanges.spec.ts
@@ -1,8 +1,92 @@
 import 'jest';
 import './helpers/mockFirebaseSetup';
+import { Request, Response } from 'express';
+import * as processTaskChanges from '../src/processTaskChanges';
+import { rollbar } from '../src/admin';
+
 
 describe('Module: processTaskChanges', () => {
-    it('placeholder', () => {
-        expect(true).toEqual(true);
+    describe('Function: processTaskChanges', () => {
+        const todoistId = 'test-id';
+        let req: Request;
+        let res: Response;
+        let statusMock: jest.MockedFunction<any>;
+        let sendMock: jest.MockedFunction<any>;
+        let topicMock: jest.MockedFunction<any>;
+        let publishMock: jest.MockedFunction<any>;
+
+
+        beforeEach(() => {
+            publishMock = jest.fn().mockResolvedValue({});
+            topicMock = jest.fn().mockReturnValue({
+                publish: publishMock,
+            });
+
+            jest.spyOn(processTaskChanges, 'getPubSub')
+                .mockReturnValue({
+                    topic: topicMock,
+                } as any);
+
+            sendMock = jest.fn();
+            statusMock = jest.fn().mockReturnValue({
+                send: sendMock,
+            });
+
+            req = {
+                body: {
+                    user_id: todoistId,
+                }
+            } as Request;
+
+            res = {
+                status: statusMock,
+            } as Response;
+        });
+
+        afterEach(() => {
+            jest.restoreAllMocks();
+        });
+
+        it('should send 400 status if no user_id', async () => {
+            delete req.body.user_id;
+            await processTaskChanges.processTaskChanges(req, res);
+
+            expect(statusMock).toHaveBeenCalledWith(400);
+            expect(sendMock).toHaveBeenCalled();
+        });
+
+        it('should send 200 status otherwise', async () => {
+            await processTaskChanges.processTaskChanges(req, res);
+
+            expect(statusMock).toHaveBeenCalledWith(200);
+            expect(sendMock).toHaveBeenCalled();
+        });
+
+        it('should call topic with todoist-updates', async () => {
+            await processTaskChanges.processTaskChanges(req, res);
+
+            expect(topicMock).toHaveBeenCalledWith('todoist-updates');
+        });
+
+        it('should call publish with data', async () => {
+            await processTaskChanges.processTaskChanges(req, res);
+
+            const expected = Buffer.from(
+                JSON.stringify({
+                    todoistId,
+                })
+            );
+
+            expect(publishMock).toHaveBeenCalledWith(expected);
+        });
+
+        it('should call rollbar with error', async () => {
+            publishMock.mockRejectedValue({});
+
+            const rollbarSpy = jest.spyOn(rollbar, 'error');
+
+            await processTaskChanges.processTaskChanges(req, res);
+            expect(rollbarSpy.mock.calls[0][0]).toMatch(/Failed to publish user ID/);
+        });
     });
 });
