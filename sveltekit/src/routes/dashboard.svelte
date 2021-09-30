a811617a6507757ea26e8976672ec2bed1189e24
Ben Crane <bencrane@protonmail.com>
2021-09-30 03:31:47 +0000

    work in progress

--- sveltekit/src/routes/dashboard.svelte
+++ sveltekit/src/routes/dashboard.svelte
@@ -7,6 +7,21 @@
 </script>
 
 <script lang="ts">
+	import { collection, query, getFirestore, limit, getDocs } from '@firebase/firestore';
+	import { onMount } from 'svelte';
+
+	let recentEscalatedTasks = [];
+
+	const db = getFirestore();
+
+	const userId: string = '2On8T7i7rjgtumcfgJtsNPOOkD12';
+
+	onMount(async () => {
+		const escalatedTasksRef = collection(db, 'users', userId, 'escalatedTasks');
+		const q = query(escalatedTasksRef, limit(20));
+		const documentSnapshots = await getDocs(q);
+		console.log(documentSnapshots[0]);
+	});
 </script>
 
 <div>
